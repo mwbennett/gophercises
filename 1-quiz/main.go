17ea2bcb7a979338f369aa298bb51e403a723883
Mark Bennett <mark.bennett@samsara.com>
2019-12-24 11:17:27 -0800

    1-quiz: part 1 complete

--- 1-quiz/main.go
+++ 1-quiz/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
 )
@@ -18,9 +19,9 @@ func ask(number int, question []string) bool {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Printf("Question %v: %v\n", number, question[0])
 	scanner.Scan()
-	text := scanner.Text()
+	answer := scanner.Text()
 
-	return question[1] == text
+	return question[1] == answer
 }
 
 func playQuiz(questions [][]string) (int, int) {
@@ -37,18 +38,21 @@ func playQuiz(questions [][]string) (int, int) {
 }
 
 func main() {
-	csvFile, err := os.Open("./problems.csv")
+	// Set up command line flags.
+	fileNamePtr := flag.String("file_name", "problems.csv", "the file name (with extension) where the questions are located")
+	flag.Parse()
+
+	filePath := fmt.Sprintf("./%v", *fileNamePtr)
+	csvFile, err := os.Open(filePath)
 	defer csvFile.Close()
 
 	if err != nil {
-		fmt.Println("Unable to open problems.csv, goodbye!")
+		fmt.Printf("Unable to open problems.csv, goodbye! %v\n", err)
 		return
 	}
 
 	csvReader := csv.NewReader(csvFile)
-
 	rows, err := csvReader.ReadAll()
-
 	if err != nil {
 		fmt.Println("Error reading rows from csv.")
 		return
