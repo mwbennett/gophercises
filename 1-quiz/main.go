6da7788fe92fe19a49ea19e5bd52e3b217c12fa0
Mark Bennett <mark.bennett@samsara.com>
2019-12-24 13:59:49 -0800

    1-quiz: part 2 complete

--- 1-quiz/main.go
+++ 1-quiz/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 /*
@@ -15,31 +16,25 @@ Flags:
  - limit (pt 2)
 */
 
-func ask(number int, question []string) bool {
+func endGame(reason string, score int, totalPossible int) {
+	fmt.Println(reason)
+	fmt.Printf("Your score: %v/%v\n", score, totalPossible)
+	return
+}
+
+func confirmReady(timeLimit int) bool {
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Printf("Question %v: %v\n", number, question[0])
+	fmt.Printf("Are you ready? Press ENTER to start the %v second timer and see the first question.", timeLimit)
 	scanner.Scan()
 	answer := scanner.Text()
 
-	return question[1] == answer
-}
-
-func playQuiz(questions [][]string) (int, int) {
-	score := 0
-
-	for idx, question := range questions {
-		correct := ask(idx+1, question)
-		if correct {
-			score++
-		}
-	}
-
-	return score, len(questions)
+	return answer == ""
 }
 
 func main() {
 	// Set up command line flags.
 	fileNamePtr := flag.String("file_name", "problems.csv", "the file name (with extension) where the questions are located")
+	timeLimitPtr := flag.Int("time_limit", 30, "time limit in seconds")
 	flag.Parse()
 
 	filePath := fmt.Sprintf("./%v", *fileNamePtr)
@@ -52,13 +47,43 @@ func main() {
 	}
 
 	csvReader := csv.NewReader(csvFile)
-	rows, err := csvReader.ReadAll()
+	questions, err := csvReader.ReadAll()
 	if err != nil {
-		fmt.Println("Error reading rows from csv.")
+		fmt.Println("Error reading questions from csv.")
 		return
 	}
 
-	correct, possible := playQuiz(rows)
+	if !confirmReady(*timeLimitPtr) {
+		fmt.Println("Exiting, goodbye!")
+		return
+	}
 
-	fmt.Printf("Your score: %v/%v\n", correct, possible)
+	totalPossible := len(questions)
+	timer := time.NewTimer(time.Duration(*timeLimitPtr) * time.Second)
+	score := 0
+	endGameReason := "You did it! "
+
+	AskQuestions:
+		for idx, question := range questions {
+			fmt.Printf("Question %v: %v\n", idx + 1, question[0])
+
+			answerChan := make(chan string)
+			go func() {
+				scanner := bufio.NewScanner(os.Stdin)
+				scanner.Scan()
+				answerChan <- scanner.Text()
+			}()
+
+			select {
+			case answer := <-answerChan:
+				if answer == question[1] {
+					score++
+				}
+			case <-timer.C:
+				endGameReason = "Time's up! "
+				break AskQuestions
+			}
+		}
+
+	endGame(endGameReason, score, totalPossible)
 }
